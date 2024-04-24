b3386fdb03ce9b9f540be9df416e2fdbac37e5a4
TimofeyIurkin <tturka239@gmail.com>
2024-04-24 18:39:55 +0300

    Перенёс хеширование на bcrypt для попытки решения проблем с хостингом.

--- data/user_model.py
+++ data/user_model.py
@@ -1,7 +1,7 @@
 import sqlalchemy
+import bcrypt
 from flask_login import UserMixin
 from sqlalchemy_serializer import SerializerMixin
-from werkzeug.security import generate_password_hash, check_password_hash
 from .db_session import SqlAlchemyBase
 
 
@@ -16,7 +16,7 @@ class User(SqlAlchemyBase, SerializerMixin, UserMixin):
     test = sqlalchemy.orm.relationship('Test', back_populates='user')
 
     def set_password(self, password):
-        self.hashed_password = generate_password_hash(password)
+        self.hashed_password = bcrypt.hashpw(password.encode(), bcrypt.gensalt())
 
     def check_password(self, password):
-        return check_password_hash(self.hashed_password, password)
+        return bcrypt.checkpw(password.encode(), self.hashed_password)
